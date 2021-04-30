a35b48267645b5af20a98bdd912f8fc973820975
joshua ayomide <4surejosh@gmail.com>
2021-04-30 17:43:41 +0200

    commit

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
-const bcrypt = require ('bcrypt');
+const bcrypt = require ('bcryptjs');
 const shortid = require('shortid');
 
 
